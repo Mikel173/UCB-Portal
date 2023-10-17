f29b11397d03c357a21206b2a673fd0989bc1fcf
andrea <andrea.quiroga@ucb.edu.bo>
2023-10-16 21:59:34 -0400

    Noticias

--- src/App.js
+++ src/App.js
@@ -5,17 +5,29 @@ import React, { Component } from 'react';
 import {ServicioEventos} from './servicios/ServicioEventos';
 import NavbarComponent from './componentes/Navbar';
 import { Container } from 'react-bootstrap';
+import {ServicioNoticias} from './servicios/ServicioNoticias';
+import CardNews from './componentes/CardNews';
 
 export default class App extends Component {
   constructor(){
     super();
-    this.state = {};
+    this.state = {
+      events: [],
+      news: [],
+    };
     this.servicioEventos = new ServicioEventos();
+    this.servicioNoticias = new ServicioNoticias();
   }
 
+
   componentDidMount() {
-    this.servicioEventos.getAll().then(data => {
-      this.setState({ events: data.data }); // Accede al arreglo dentro de la propiedad "data"
+    this.servicioEventos.getAll().then((data) => {
+      this.setState({ events: data.data });
+    });
+
+    this.servicioNoticias.getAll().then((data) => {
+      console.log(data.data);
+      this.setState({ news: data.data });
     });
   }
 
@@ -42,8 +54,23 @@ export default class App extends Component {
                 description={event.descripcion}
               />
             </div>
-
           ))}
+           
+          <Container className='titulos'>
+            <h2>Noticias</h2>
+          </Container>
+
+          {this.state.news && this.state.news.map(news => (
+            <div key={news.noticiaId} className="card-container">
+              <CardNews
+                uwu={news.noticiaId}
+                key={news.noticiaId}
+                title={news.titulo}
+                description={news.contenido}
+              />
+            </div>
+          ))}
+
         </div>
       </div>
     );
