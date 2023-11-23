35a9039333950baf160b9bb220547027d7dea9c1
Mikel173 <lalolandia173@gmail.com>
2023-11-22 23:00:25 -0400

    Sistema virtuales agregados

--- src/paginas/PlataformaEnLinea.js
+++ src/paginas/PlataformaEnLinea.js
@@ -1,7 +1,7 @@
 import React from "react";
-import NavbarComponent from '../componentes/Navbar';
 import ImagePanel from '../componentes/ImagePanel/ImagePanel';
 import Panel from '../componentes/Panel/Panel';
+import SistemasVirtuales from '../componentes/SistemasVirtuales'; // Asegúrate de ajustar la ruta correcta
 
 const PlataformaEnLinea = () => {
     return (
@@ -10,13 +10,7 @@ const PlataformaEnLinea = () => {
             <ImagePanel title="Plataforma de Aprendizaje en Línea" />
             <Panel />
             {/* Contenido de la página de Plataforma en Línea */}
-            <div className="container mt-5">
-                <div className="text-center">
-                    <h2 style={{ color: 'purple', fontSize: '3rem' }}>¡Únete ahora!</h2>
-                    <p style={{ color: 'purple', fontSize: '1.5rem' }}>Explora nuestra plataforma de aprendizaje y transforma tu experiencia educativa. ¡No esperes más!</p>
-                    <a href="https://neo.ucb.edu.bo/" className="btn btn-success btn-lg">Unirse</a>
-                </div>
-            </div>
+            <SistemasVirtuales />
         </div>
     );
 }
