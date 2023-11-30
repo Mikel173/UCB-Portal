ac48d7c2c13d38148ce33ca7b010dc7af8e24934
Mike173 <lalolandia173@gmail.com>
2023-11-30 03:07:07 -0400

    Paginas faltantes

--- src/componentes/Componente.js
+++ src/componentes/Componente.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css';
 import RE001 from '../imagenes/RE001.png';
 import RE002 from '../imagenes/RE002.png';
-import RE003 from '../imagenes/RE003.png';
+import RE003 from '../imagenes/feria.png';
 import RE004 from '../imagenes/RE004.png';
 import RE005 from '../imagenes/RE005.png';
 import { Link } from 'react-router-dom';
@@ -12,7 +12,10 @@ return (
     <div className="componente">
         <img src={RE001} alt="Imagen 1" />
         <div className="descripcion">
-        <h2 className='Subtitulos'>Talleres de Empleabilidad</h2>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSeXc9mozt1UL_rux4rMIAaGEZb6qQGgteZLBVg4ZM-ugYNkbw/viewform" target="_blank" rel="noopener noreferrer">
+            <h2 className='Subtitulos'>Talleres de Empleabilidad</h2>
+        </a>
+
         <p>Complementando tu formación académica, la USEI te apoya con el desarrollo de competencias laborales para que tengas una favorable transición al mercado laboral.</p>
         </div>
     </div>
@@ -20,7 +23,10 @@ return (
     <div className="componente">
         <img src={RE002} alt="Imagen 2" />
         <div className="descripcion">
-        <h2 className='Subtitulos'>Encuesta a tiempo de graduación</h2>
+       
+        <a href="http://encuestasacademicas.ucb.edu.bo/limesurvey/index.php/663945?lang=es" target="_blank" rel="noopener noreferrer">
+            <h2 className='Subtitulos'> <Link to="/programa-empleabilidad" className='Subtitulos'>Encuesta a tiempo de graduación</Link></h2>
+        </a>
         <p>Registrate para recibir más información profesional.</p>
         </div>
     </div>
@@ -28,18 +34,11 @@ return (
     <div className="componente">
         <img src={RE003} alt="Imagen 3" />
         <div className="descripcion">
-        <h2 className='Subtitulos'>Bolsa de Trabajo</h2>
-        <p>Los convenios con instituciones públicas y privadas te permiten realizar pasantías que te permitirán adquirir experiencia laboral para iniciar tu vida personal.</p>
-        </div>
+        <h2 className='Subtitulos'><Link to="/feria-empleo" className='Subtitulos'>Feria del empleo UCB</Link></h2>
+        <p>Nuevamente la Universidad Católica Boliviana “San Pablo” (U.C.B.), organiza a nivel nacional, del 10 al 28 de julio de 2023, la tercera versión de la “Ferial del empleo UCB”.</p>
+        </div>  
     </div>
 
-    <div className="componente">
-        <img src={RE004} alt="Imagen 4" />
-        <div className="descripcion">
-        <h2 className='Subtitulos'>Feria de Empleo</h2>
-        <p>La Feria del Empleo UCB es un evento que concentra la oferta y demanda de puestos laborales, facilita el contacto entre instituciones empleadoras y personas interesadas en obtener un trabajo.</p>
-        </div>
-    </div>
 
     <div className="componente">
         <img src={RE005} alt="Imagen 5" />
